6695faab400ccded1752a02e7101db96275a8282
JonathanBY15 <bariscanyaprak@gmail.com>
2025-03-10 18:21:46 -0400

    Added style to ChallengeCard and ChallengeList

--- src/components/MainPage/ChallengeCard/ChallengeCard.tsx
+++ src/components/MainPage/ChallengeCard/ChallengeCard.tsx
@@ -19,18 +19,16 @@ const ChallengeCard: React.FC<ChallengeCardProps> = ({ challenge }) => {
   const navigate = useNavigate();
 
   return (
-<li
-  className="bg-[#161616a6] rounded-md p-3 cursor-pointer 
-             transition-all duration-200 list-none flex flex-col justify-between aspect-[5/2] sm:aspect-[4/2] lg:aspect-[3/2] 
-             relative hover:bg-[#16161641] 
-             hover:shadow-[0_-4px_8px_0_rgba(59,130,246,0.3),0_4px_8px_0_rgba(59,130,246,0.3),0_0_8px_0_rgba(59,130,246,0.3)] 
-             group"
-  onClick={() => navigate("/code")}
->
-
-
+      <li
+        className="bg-[#161616a6] rounded-md p-3 cursor-pointer 
+                  transition-all duration-200 list-none flex flex-col justify-between aspect-[5/2] sm:aspect-[4/2] lg:aspect-[3/2] 
+                  relative hover:bg-[#16161641] 
+                  hover:shadow-[0_-4px_8px_0_rgba(59,130,246,0.3),0_4px_8px_0_rgba(59,130,246,0.3),0_0_8px_0_rgba(59,130,246,0.3)]  gap-2
+                  group"
+        onClick={() => navigate("/code")}
+      >
       {/* Top Section */}
-      <div className="flex justify-between text-white text-xs font-semibold">
+      <div className="flex justify-between text-white font-semibold">
         <span>{challenge.number}. {challenge.name}</span>
         <span className={`flex items-center gap-1 px-2 py-1 rounded-md text-xs ${getBadgeColor(challenge.difficulty)}`}>
           <PiFire className="text-sm text-amber-50" /> {challenge.difficulty}
@@ -38,7 +36,7 @@ const ChallengeCard: React.FC<ChallengeCardProps> = ({ challenge }) => {
       </div>
 
       {/* Middle Section */}
-      <p className="text-gray-300 text-xs text-start flex-1 flex items-center justify-center px-0 leading-tight">
+      <p className="text-gray-300 text-start flex-1 flex justify-center px-0 leading-tight">
         {challenge.description || "Solve this challenge to test your skills! Lorem Lorem ipsum Lorem lorem sssa sads."}
       </p>
 
--- src/components/MainPage/ChallengeList/ChallengeList.tsx
+++ src/components/MainPage/ChallengeList/ChallengeList.tsx
@@ -7,8 +7,7 @@ interface ChallengeListProps {
 
 const ChallengeList: React.FC<ChallengeListProps> = ({ challenges }) => {
   return (
-    // <ul className="w-full max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-1 lg:gap-4 p-4">
-    <ul className="w-full max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-1 lg:gap-4 p-4">
+    <ul className="w-full max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-1 lg:gap-4 p-4">
 
       {challenges.map((challenge) => (
         <ChallengeCard key={challenge.number} challenge={challenge} />
