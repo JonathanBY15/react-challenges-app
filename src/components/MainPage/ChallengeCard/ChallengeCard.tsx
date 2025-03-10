1b2422b8fc467531b38031f28cdd1525dbe0ea48
JonathanBY15 <bariscanyaprak@gmail.com>
2025-03-10 19:52:13 -0400

    Made UL and LI responsive

--- src/components/MainPage/ChallengeCard/ChallengeCard.tsx
+++ src/components/MainPage/ChallengeCard/ChallengeCard.tsx
@@ -21,7 +21,7 @@ const ChallengeCard: React.FC<ChallengeCardProps> = ({ challenge }) => {
   return (
       <li
         className="bg-[#161616a6] rounded-md p-3 cursor-pointer 
-                  transition-all duration-200 list-none flex flex-col justify-between aspect-[5/2] sm:aspect-[4/2] lg:aspect-[3/2] 
+                  transition-all duration-200 list-none flex flex-col justify-between aspect-[6/2] sm:aspect-[4/2] lg:aspect-[3/2] 
                   relative hover:bg-[#16161641] 
                   hover:shadow-[0_-4px_8px_0_rgba(59,130,246,0.3),0_4px_8px_0_rgba(59,130,246,0.3),0_0_8px_0_rgba(59,130,246,0.3)]  gap-2
                   group"
--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -61,7 +61,7 @@ const MainPage: React.FC = () => {
 
   return (
     // <div className="flex flex-col justify-start items-center min-h-screen bg-[#282828] w-full p-[96px] relative">
-    <div className="flex flex-col justify-start items-center min-h-screen bg-[#282828] w-full p-[96px] relative overflow-auto">
+    <div className="flex flex-col justify-start items-center min-h-screen bg-[#282828] w-full pt-[88px] px-[40px] relative overflow-auto gap-8">
 
       <GradientBlob size={300} top="10%" left="10%" />
       <GradientBlob size={250} bottom="20%" right="15%" opacity={0.5} animationDelay="1s" />
