6b0f276a551323973f0ec1019e41362eee13c958
JonathanBY15 <bariscanyaprak@gmail.com>
2025-03-16 10:13:50 -0400

    Updated challenge-description-window minWidth to account for scroll bar

--- src/components/CodeEditor/CodeEditorDesktop/CodeEditorDesktop.tsx
+++ src/components/CodeEditor/CodeEditorDesktop/CodeEditorDesktop.tsx
@@ -81,8 +81,8 @@ const CodeEditorDesktop = () => {
     <SandpackProvider files={files} theme={monokaiPro} template="react">
       {/* <Navbar></Navbar> */}
       <SandpackLayout>
-        <Split className="split" minSize={302} snapOffset={0} sizes={sizesHorizontal} onDragEnd={handleDragHorizontal}>
-          {/** minSize=302 prevents ChallengeNavbar from losing style */}
+        <Split className="split" minSize={314} snapOffset={0} sizes={sizesHorizontal} onDragEnd={handleDragHorizontal}>
+          {/** minSize=314 prevents ChallengeNavbar from losing style */}
           <div className="challenge-description-window">
             {/* Challenge Description Pane */}
             <ChallengeInfo challenge={challengeData} />
