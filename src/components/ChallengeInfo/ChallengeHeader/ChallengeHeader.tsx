3f575ba06b91f0512deaa5d43da292195ea45e47
JonathanBY15 <bariscanyaprak@gmail.com>
2025-03-18 10:39:39 -0400

    Updated ChallengeHeader to be responsive with useElementWidth

--- src/components/ChallengeInfo/ChallengeHeader/ChallengeHeader.tsx
+++ src/components/ChallengeInfo/ChallengeHeader/ChallengeHeader.tsx
@@ -1,6 +1,8 @@
 /**
  * Displays the header for a coding challenge, including its id(challenge number), name, and difficulty level.
  * The difficulty badge dynamically applies a CSS class based on the difficulty level.
+ * 
+ * Uses useElementWidth to track width of header and assign responsive classes.
  *  
  * @component
  * @param {number} id - The unique identifier for the challenge (challenge number).
@@ -9,8 +11,9 @@
  * @returns {JSX.Element} The rendered challenge header component.
  */
 
-import React from "react";
+import React, { useRef } from "react";
 import { PiFire } from "react-icons/pi";
+import useElementWidth from "../../../hooks/useElementWidth";
 import "./ChallengeHeader.css";
 
 interface ChallengeHeaderProps {
@@ -20,12 +23,23 @@ interface ChallengeHeaderProps {
 }
 
 const ChallengeHeader: React.FC<ChallengeHeaderProps> = ({ id, name, difficulty }) => {
+  const headerRef = useRef<HTMLDivElement | null>(null);
+  const width = useElementWidth(headerRef);
+
+  // Determine header class based on width
+  let sizeClass = "large-header"; // Default
+  if (width < 350) sizeClass = "ex-small-header";
+  else if (width < 388) sizeClass = "small-header";
+  else if (width < 455) sizeClass = "medium-header";
+
   return (
-    <div className="challenge-header">
+    <div ref={headerRef} className={`challenge-header ${sizeClass}`}>
       <h1>
         {id}. {name}
       </h1>
-      <span className={`badge badge-${difficulty.toLowerCase()}`}><PiFire /> {difficulty}</span>
+      <span className={`badge badge-${difficulty.toLowerCase()}`}>
+        <PiFire /> {difficulty}
+      </span>
     </div>
   );
 };
